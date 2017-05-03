a00b33441b4d68b1b2950ade1eadd796c825be15
Parnian <parnian@parnians-mbp.home>
2017-05-03 16:29:58 +0200

    overflowalert fixed

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -28,9 +28,13 @@ class TweetBox extends PureComponent {
 
   overFlowAlert() {
     if (this.remainingCharacters() < 0) {
-      var beforeOverflowText = this.state.text.substring(140 - 10, 140);
-      var overflowText = this.state.text.substring(140);
-
+      if (this.state.photoAdded) {
+        var beforeOverflowText = this.state.text.substring(140 - 23 - 10, 140 - 23);
+        var overflowText = this.state.text.substring(140 - 23);
+      } else {
+        var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+        var overflowText = this.state.text.substring(140);
+      }
       return(
         <div className="alert alert-warning">
           <strong>Oops! Too long:</strong>
